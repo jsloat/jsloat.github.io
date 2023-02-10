c875db6ac253fd431c0208260f26115ea52daa55
John Sloat <jsloat1@gmail.com>
2023-02-09 20:40:23 -0800

    More router tweaking

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <App />
     </HashRouter>
   </React.StrictMode>
