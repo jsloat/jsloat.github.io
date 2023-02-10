f1a5279bc0863a1fcdcc51931ccc35105e8c0ab4
John Sloat <jsloat1@gmail.com>
2023-02-09 20:37:17 -0800

    Routing framework

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
 import React from "react";
+import { Link, Route, Routes } from "react-router-dom";
 
-const App = () => <h1>Hello world!</h1>;
+const App = () => (
+  <>
+    <div>
+      <div>
+        <b>
+          <Link to="/a">Route A</Link>
+          <br />
+          <Link to="/b">Route B</Link>
+          <br />
+          <Link to="/c">Route C</Link>
+          <br />
+          <br />
+        </b>
+      </div>
+    </div>
+    <Routes>
+      <Route path="/" element={"home"} />
+      <Route path="/a" element={"a"} />
+      <Route path="/b" element={"b"} />
+      <Route path="/c" element={"c"} />
+    </Routes>
+  </>
+);
 
 export default App;
