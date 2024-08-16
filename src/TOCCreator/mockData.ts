24abda9f2731f391895a49bc62f25b59dd5c9d0a
John Sloat <jsloat1@gmail.com>
2024-08-16 09:19:48 -0700

    toc creator mock data update

--- src/TOCCreator/mockData.ts
+++ src/TOCCreator/mockData.ts
@@ -3,5 +3,6 @@ All entries are sorted by page number - earliest page ascending (3)
 Non-headers are sorted under headers (2)
 # Header looks like this (2,5)
 Wrap tags with the pound sign #like this# (4)
-# Printed layout leaves wider left margin for hole punching (5)
+# Printed layout leaves wider left margin for hole punching (5,10)
+Entries can have multiple page ranges (6) (7,9)
 `;
